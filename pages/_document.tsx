6e917d3ba0861c6454727dcde8660e547ea474a3
arifimran5 <arifimran505@gmail.com>
2022-05-17 22:26:02 +0530

    add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,11 +3,23 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang='en'>
         <Head>
           <link
             href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap'
             rel='stylesheet'
+          />
+          <link href='/favicon.ico' rel='shortcut icon' />
+          <link
+            href='/apple-touch-icon.png'
+            rel='apple-touch-icon'
+            sizes='180x180'
+          />
+          <link
+            href='/favicon-32x32.png'
+            rel='icon'
+            sizes='32x32'
+            type='image/png'
           />
         </Head>
         <body>
