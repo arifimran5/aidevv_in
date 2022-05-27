e3010827bb8c5bd49dfdb4952b0148e6580eb42e
arifimran5 <arifimran505@gmail.com>
2022-05-28 00:00:12 +0530

    woff2 inter font

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 ## My Personal Website with blog
 
+![aidevv home page screenshot](/public/aidevv_in_home.png)
+
 #### ⚒️ Technologies used
 
 - ⚡️ Next.JS,
 - 🪐 TailwindCSS
 - 🌈 ContentLayer with MDX
+- 🔥 Framer Motion
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,9 +5,51 @@ class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
-          <link
+          {/* <link
             href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap'
             rel='stylesheet'
+          /> */}
+          <link
+            rel='preload'
+            href='/fonts/inter/inter500.woff2'
+            as='font'
+            type='font/woff2'
+            crossOrigin='anonymous'
+          />
+          <link
+            rel='preload'
+            href='/fonts/inter/inter600.woff2'
+            as='font'
+            type='font/woff2'
+            crossOrigin='anonymous'
+          />
+          <link
+            rel='preload'
+            href='/fonts/inter/inter700.woff2'
+            as='font'
+            type='font/woff2'
+            crossOrigin='anonymous'
+          />
+          <link
+            rel='preload'
+            href='/fonts/inter/inter800.woff2'
+            as='font'
+            type='font/woff2'
+            crossOrigin='anonymous'
+          />
+          <link
+            rel='preload'
+            href='/fonts/inter/inter900.woff2'
+            as='font'
+            type='font/woff2'
+            crossOrigin='anonymous'
+          />
+          <link
+            rel='preload'
+            href='/fonts/inter/inter-regular.woff2'
+            as='font'
+            type='font/woff2'
+            crossOrigin='anonymous'
           />
           <link href='/favicon-48x48.png' rel='shortcut icon' />
           <link
